3c85f73c57d4f30c78e2aa8ff1684e0ce56eed49
ikyrie <mah.c5r@gmail.com>
2022-12-20 20:59:40 -0300

    Video 3.2

--- lib/screens/drink_menu.dart
+++ lib/screens/drink_menu.dart
@@ -8,14 +8,25 @@ final List items = drinks;
 
   @override
   Widget build(BuildContext context){
-    return GridView.builder(
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
+      child: CustomScrollView(
+        slivers: <Widget>[
+          const SliverToBoxAdapter(child: Padding(padding: EdgeInsets.only(bottom: 16.0), child: Text("Bebidas", textAlign: TextAlign.center, style: TextStyle(fontFamily: "Caveat", fontSize: 32,),),),),
+          SliverGrid(
+              delegate: SliverChildBuilderDelegate((context, index) {
+                return DrinkItem(
+                    imageURI: items[index]['image'],
+                    itemTitle: items[index]['name'],
+                    itemPrice: items[index]['price']);
+                },
+                childCount: items.length,
+              ),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+              ))
+        ],
       ),
-      itemBuilder: (context, index) {
-        return DrinkItem(imageURI: items[index]['image'], itemTitle: items[index]['name'], itemPrice: items[index]['price']);
-      },
-      itemCount: items.length,
     );
   }
 }
